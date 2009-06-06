ce7a9adc0f1690f7c3b2ec7a918b8d9b93b04872
Porn Starin <pornstarin@gmail.com>
2009-06-06 15:10:28 -0700

    added important files

---
+++ src/gameloop.c
@@ -0,0 +1,51 @@
+#include "gameloop.h"
+#include "gamestatemanager.h"
+#include "level_demo.h"
+
+void GameLoop( void )
+{
+    GS_Initialize( &GS_level_demo );
+    FrameRate_Init( 60 );    // Set max frame rate
+
+	while( GS_current != GS_QUIT )
+	{
+		// reset the system modules
+        //System::Reset();
+
+		// If not restarting, load the gamestate
+        if( GS_current != GS_RESTART )
+		{
+            //GameStateManager::Update();
+            GS_current->Load();
+		}
+		else
+			GS_next = GS_current = GS_previous;
+
+		// Initialize the gamestate
+        GS_current->Init();
+        FrameRate_Reset(); // reset the clock
+
+		while( GS_current == GS_next )
+		{
+            //pGraphics->BeginFrame();
+            //Input::Update();
+            GS_current->Update();
+            GS_current->Draw();
+			//pGraphics->EndFrame();
+
+            FrameRate_Wait();
+            //pWindow->Swap();
+            //System::Update();
+		}
+
+        GS_current->Free();
+
+		if(GS_next != GS_RESTART)
+            GS_current->Unload();
+
+		GS_previous = GS_current;
+		GS_current = GS_next;
+	}
+	
+	FrameRate_Exit();
+}
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "gameloop.h"
+#include "gamestatemanager.h"
+#include "level_demo.h"
+
+
+int main(int argc, char** argv)
+{
+    printf("\nFuck you, world!\n\n");
+    
+    GameLoop();
+    
+    return 0;
+}
